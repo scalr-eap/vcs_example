847afe745e4528710ab8ebceadd6cb64de6ce3b5
Ryan <ryan.fee625@gmail.com>
2020-04-07 13:29:09 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,8 +2,24 @@ provider "aws" {
   region     = "us-east-1"
 }
 
+data "aws_ami" "the_ami" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-bionic-18.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
 resource "aws_instance" "scalr" {
-  ami                    = "ami-2757f631"
+  ami                    = "${data.aws_ami.the_ami.id}"
   instance_type          = "t2.nano"
   subnet_id              = "subnet-0ebb1058ad727cfdb"
   vpc_security_group_ids = ["sg-0880cfdc546b123ba"]
