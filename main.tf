7be6ad2ff2b336eb845ccfdba298cca929c9c570
Ryan <ryan.fee625@gmail.com>
2020-04-09 11:03:28 -0400

    Updated instance

--- main.tf
+++ main.tf
@@ -19,7 +19,7 @@ data "aws_ami" "the_ami" {
 }
 
 resource "aws_instance" "scalr" {
-  ami                    = "${data.aws_ami.the_ami.id}"
+  ami                    = "data.aws_ami.the_ami.id"
   instance_type          = "t2.nano"
   subnet_id              = "subnet-0ebb1058ad727cfdb"
   vpc_security_group_ids = ["sg-0880cfdc546b123ba"]
